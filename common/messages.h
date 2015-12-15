b61f43f228950505ef809dccd6a73aefdfd44ce1
Marc-Andre Lureau <marcandre.lureau@gmail.com>
2015-12-16 00:27:23 +0100

    Add GL scanout structures
    See related protocol changes.
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@gmail.com>
    Acked-by: Frediano Ziglio <fziglio@redhat.com>

--- common/messages.h
+++ common/messages.h
@@ -379,6 +379,9 @@ typedef struct SpiceMsgcDisplayStreamReport {
     uint32_t audio_delay;
 } SpiceMsgcDisplayStreamReport;
 
+typedef struct SpiceMsgcDisplayGlDrawDone {
+} SpiceMsgcDisplayGlDrawDone;
+
 typedef struct SpiceMsgCursorInit {
     SpicePoint16 position;
     uint16_t trail_length;
@@ -637,6 +640,22 @@ typedef struct SpiceMsgcPortEvent {
 typedef struct SpiceMsgcDisplayPreferredCompression {
     uint8_t image_compression;
 } SpiceMsgcDisplayPreferredCompression;
+
+typedef struct SpiceMsgDisplayGlScanoutUnix {
+    int drm_dma_buf_fd;
+    uint32_t width;
+    uint32_t height;
+    uint32_t stride;
+    uint32_t drm_fourcc_format;
+    uint32_t flags;
+} SpiceMsgDisplayGlScanoutUnix;
+
+typedef struct SpiceMsgDisplayGlDraw {
+    uint32_t x;
+    uint32_t y;
+    uint32_t w;
+    uint32_t h;
+} SpiceMsgDisplayGlDraw;
 
 SPICE_END_DECLS
 
